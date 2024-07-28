44bc631a0f5acf9a0fe53c76b05b457985622ac7
chirag jindal <golujindal76@gmail.com>
2024-07-28 15:50:57 +0530

    commit

--- robot.cpp
+++ robot.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
 int main() {
-    string answer;
-    cout << "Are You a Robot, Again? (yes/no): ";
-    cin >> answer;
-
-    cout << (answer == "No" ? "Yes, I am a robot." : "No, I'm not a robot.") << endl;
-
+    int n = 121;
+    int temp = n;
+    int ans = 0;
+    
+    while (n != 0) {
+        int digit = n % 10;
+        ans = ans * 10 + digit;
+        n = n / 10;
+    }
+    
+    if (temp == ans) {
+        cout << "Yes, it is a palindrome number." << endl;
+    } else {
+        cout << "No, it is not a palindrome number." << endl;
+    }
+    
     return 0;
 }
